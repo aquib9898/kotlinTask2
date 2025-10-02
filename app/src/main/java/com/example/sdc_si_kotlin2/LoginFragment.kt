99451938bccb8c13224b01426f3b387aeb66ef64
aquib9898 <noorshab71@gmail.com>
2025-10-02 15:58:51 +0530

    Added fragment login and register fragment

---
+++ app/src/main/java/com/example/sdc_si_kotlin2/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.example.sdc_si_kotlin2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val createAccountLink: TextView = view.findViewById(R.id.tv_create_account_link)
+        createAccountLink.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, RegisterFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sdc_si_kotlin2/RegisterFragment.kt
@@ -0,0 +1,33 @@
+package com.example.sdc_si_kotlin2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navigate back to LoginFragment when "Login here" is clicked
+        val loginHereLink: TextView = view.findViewById(R.id.tv_login_link)
+        loginHereLink.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LoginFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+}
